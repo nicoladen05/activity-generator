97d66c19edd407bb1bf8364f07904ea908fa4dcf
nicoladen05 <nicolashartmanntaba@gmail.com>
2025-07-25 16:18:14 +0200

    new prompts

--- src/app/api/prompts.js
+++ src/app/api/prompts.js
@@ -1,8 +1,8 @@
 export const prompts = {
   drawing:
-    "Generate two words that are related to drawing. The first word should be a noun, and the second word should be an adjective that describes the noun. For example, if the first word is 'pencil', the second word could be 'sharp'.",
+    "Deine Aufgabe ist es, zwei Wörter zu generieren, die fuer die Aufgabe Zeichnen in Activity genutzt werden koennen. Diese Woerter sind meist zusammengesetzte woerter, wie beispielsweise 'Tiergarten'.",
   explaining:
-    "Generate two words that are related to explaining. The first word should be a noun, and the second word should be a verb that describes an action related to the noun. For example, if the first word is 'concept', the second word could be 'clarify'.",
+    "Deine Aufgabe ist es, zwei Wörter zu generieren, die fuer die Aufgabe Erklaeren in Activity genutzt werden koennen. Diese Woerter sind meist zusammengesetzte woerter, wie beispielsweise 'Tiergarten'.",
   acting:
-    "Generate two words that are related to acting. The first word should be a noun, and the second word should be an adverb that describes how the noun is performed. For example, if the first word is 'performance', the second word could be 'dramatically'.",
+    "Deine Aufgabe ist es, zwei Wörter zu generieren, die fuer die Aufgabe Vorspielen in Activity genutzt werden koennen. Diese Woerter sind meist zusammengesetzte woerter, wie beispielsweise 'Tiergarten'. Bitte achte darauf dass diese woerter bzw die wortbestandteile zum vorspielen geeignet sind.",
 };
